f4ae4674d4464168750c8e3bf22dcd8504c2c5f3
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2021-10-08 16:04:50 -0700

    Create Liftoff!.py

---
+++ Liftoff!.py
@@ -0,0 +1,28 @@
+Write a program in the file liftoff.py that prints out the calls for a spaceship that is about to launch. Countdown from 10 to 1 and then output Liftoff!
+
+Your program should use a for loop.
+
+Here's a sample run of the program:
+
+$ python liftoff.py
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Liftoff!
+
+-------
+
+def main():
+    for i in range(10,0,-1):
+        print(i)
+    print("Liftoff!")
+
+if __name__ == '__main__':
+    main()
